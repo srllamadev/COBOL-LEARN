184a5e60bbf31720fc524f0daee0645832739a26
SrLLamaDev <srllamadev@gmail.com>
2025-07-18 17:17:19 -0400

    testCode Cobol

---
+++ 1_Intro/HOLA.cbl
@@ -0,0 +1,8 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. HOLA.
+       ENVIRONMENT DIVISION.
+       DATA DIVISION.
+       PROCEDURE DIVISION.
+           DISPLAY "Hola mundo desde COBOL uwu".
+           DISPLAY "Hola ".
+           STOP RUN.
